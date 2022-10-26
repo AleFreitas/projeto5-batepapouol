a43c560276585ec2bcbe773f97d2cc676ea61ccc
Alexandre <alexandrefsoficial@gmail.com>
2022-10-25 22:07:32 -0300

    basic logic for axios requests added in script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let userName = prompt("Insira seu nome de usuário");
+console.log(userName)
+postUserName()
+getMessages()
+function postUserName(){
+    //tenta postar o nome do usuário
+    //se não existir alerta seja bem vindo
+    //se existir atualiza userName e chama a função de novo
+}
+function getMessages(){
+    //busca as mensagens para mostrar
+}
+function getUsers(){
+    //retorna os usuários ativos
+}
