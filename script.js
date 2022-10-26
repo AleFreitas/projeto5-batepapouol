b6e97bd8c448072c245843f6570e9ac884ebb2c4
Alexandre <alexandrefsoficial@gmail.com>
2022-10-26 19:34:52 -0300

    user login implemented

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let userName = prompt("Insira seu nome de usuário");
-console.log(userName)
 postUserName()
 getMessages()
-function postUserName(){
+function postUserName() {
+    console.log(userName)
     //tenta postar o nome do usuário
+    user = { name: userName }
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", user)
+    promise.then(userDontExists);
+    promise.catch(userAlreadyExists);
     //se não existir alerta seja bem vindo
     //se existir atualiza userName e chama a função de novo
 }
-function getMessages(){
+function userDontExists(){
+    alert("seja bem vindo")
+}
+function userAlreadyExists() {
+    userName = prompt("Este nome está sendo usado, insira um nome válido");
+    postUserName()
+}
+function getMessages() {
     //busca as mensagens para mostrar
 }
-function getUsers(){
+function getUsers() {
     //retorna os usuários ativos
 }
