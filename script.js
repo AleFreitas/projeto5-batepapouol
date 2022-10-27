c788e9ba9e583426d263a080e5b6c9189f08d7d1
Alexandre <alexandrefsoficial@gmail.com>
2022-10-27 18:38:38 -0300

    chat lag while getting messages solved

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let userName = prompt("Insira seu nome de usuário");
 const section = document.querySelector("section")
+let messageHtml = ""
+let lastMessage = 0
 postUserName()
 function postUserName() {
     console.log(userName)
@@ -16,6 +18,7 @@ function userDontExists(){
         const keepUser = axios.post("https://mock-api.driven.com.br/api/v6/uol/status",user)
     },5000)
     setInterval(function(){
+        let messageHtml = ""
         getMessages();
     },3000)
 }
@@ -23,18 +26,18 @@ function userAlreadyExists() {
     userName = prompt("Este nome está sendo usado, insira um nome válido");
     postUserName()
 }
+//busca as mensagens para mostrar
 function getMessages() {
     const messages = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
-    messages.then(showMessages)
+    messages.then(buildMessages)
     messages.catch(didntFindMessages)
-    //busca as mensagens para mostrar
 }
-function showMessages(response){
-    section.innerHTML="";
-    console.log(response.data)
+//constroi as mensagens em uma variavel messageHtml
+function buildMessages(response){
+    messageHtml = "";
     for(i=0;i<response.data.length;i++){
         if(response.data[i].type === "status"){
-            section.innerHTML+=`
+            messageHtml+=`
             <div class="message status message${i}">
                 <p>
                     <span class="time">(${response.data[i].time})</span> <span class="name">${response.data[i].from}</span> ${response.data[i].text}
@@ -42,7 +45,7 @@ function showMessages(response){
             </div>
             `;
         }else if(response.data[i].type === "message"){
-            section.innerHTML+=`
+            messageHtml+=`
             <div class="message normal-message message${i}">
                 <p>
                     <span class="time">(${response.data[i].time})</span> <span class="name">${response.data[i].from}</span> para <span class="name">${response.data[i].to}:</span> ${response.data[i].text}
@@ -51,7 +54,7 @@ function showMessages(response){
             `;
         }
         else if(response.data[i].type === "private_message"){
-            section.innerHTML+=`
+            messageHtml+=`
             <div class="message private-message message${i}">
                 <p>
                     <span class="time">(${response.data[i].time})</span> <span class="name">${response.data[i].from}</span> reservadamente para <span class="name">${response.data[i].to}</span>: ${response.data[i].text}
@@ -59,9 +62,16 @@ function showMessages(response){
             </div>
             `;
         }
-        let showThisMessage = document.querySelector(".message"+i);
-        showThisMessage.scrollIntoView();
+        console.log(messageHtml)
+        lastMessage = i;
     }
+    showMessages();
+}
+function showMessages(){
+    section.innerHTML=messageHtml
+    console.log(messageHtml)
+    let showThisMessage = document.querySelector(".message"+lastMessage);
+    showThisMessage.scrollIntoView();
 }
 function didntFindMessages(response){
     alert("Não foi possivel obter as mensagens")
